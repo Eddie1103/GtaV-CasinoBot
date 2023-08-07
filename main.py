52618f6203b335d0905eb2a69d6ac74daa72e90f
Admin <Admin@STUDIOEDDIE>
2023-08-08 01:26:40 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import time
 
-if __name__ == __main__:
+_amount = 1000
+
+def startup():
+    print("Starting...")
+    time.sleep(30)
+
+def moveMouseTo(x, y):
+    print("")
 
 
-    
+if __name__ == "__main__":
+    startup()
+
+#maus auf rot -> max einsatz -> einsatz-counter abarbeiten (place orders)
+#detection for win/lose -> einsatz-counter verdoppeln oder zurücksetzen
