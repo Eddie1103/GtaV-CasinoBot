414c03d137624bbafa18c2e0050dfbd81f8d39e0
Eddie <89065499+Eddie1103@users.noreply.github.com>
2023-08-08 18:37:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,72 @@
 import pyautogui
-import time
+import time, os
+from pynput.mouse import Button, Controller
+from directkeys import PressKey, ReleaseKey, TAB
+from screenSize import GetScreenSize
 
-_amount = 1000
 _betCounter = 1
+SCREEN_SIZE = GetScreenSize()
+SCREEN_STRING = str(SCREEN_SIZE[0]) + "x" + str(SCREEN_SIZE[1])
+MOUSE = Controller()
 
 def startup():
+    if os.path.exists("pics/bet" + SCREEN_STRING + ".png") == False:
+        print("Kein Referenzbild für aktuelle Screen-Size gefunden: " + SCREEN_STRING +"px")
+        exit()
     print("Casino-Bot is starting...")
-    
+
     while(True):
         while(findBetField() == False):
             print("Konnte Tisch nicht finden...")
             time.sleep(5)
 
+
         while(detectWinOrLose() == False):
             print("Runde läuft momentan...")
             time.sleep(2)
+
     exit()
 
 def findBetField():
-    x, y= pyautogui.locateCenterOnScreen("redbet.png", region=(980,630,100,60))
-    if x != None:
-        pyautogui.moveTo(x, y, duration = 3)
-        pyautogui.press("tab")
-        time.sleep(0.5)
+    try:
+        point= pyautogui.locateCenterOnScreen(f'pics/bet{SCREEN_STRING}.png', confidence=0.8)
+        print("Tisch gefunden!")
+        moveMouseTo(point[0], point[1])
+
+        PressKey(TAB)
+        time.sleep(1)
+        ReleaseKey(TAB)
+
+        time.sleep(1)
         for i in range(_betCounter):
-            pyautogui.leftClick()
-            time.sleep(1)
+           print("mouse clicking...")
+           MOUSE.press(Button.left)
+           time.sleep(1)
+           MOUSE.release(Button.left)
+           time.sleep(1)
         return True
-    return False
+    except:
+        return False
 
 def detectWinOrLose():
-    x, y= pyautogui.locateCenterOnScreen("win.png", region=(0,0,370,90))
-    if x == None:
-        x,y = pyautogui.locateCenterOnScreen("lose.png", region=(0,0,370,90))
-        if x == None:
-            return False
-        else:
-            _betCounter*2
-    else:
+    try:
+        x, y= pyautogui.locateCenterOnScreen(f'pics/win{SCREEN_STRING}.png', confidence=0.8)
         _betCounter = 1
+        print("Gewonnen! Bet-Counter auf 1 gesetzt")
+    except:
+        try:
+            x,y = pyautogui.locateCenterOnScreen(f'pics/lose{SCREEN_STRING}.png', confidence=0.8)
+            _betCounter*=2
+            print("Verloren! Bet-Counter verdoppelt auf " + _betCounter)
+        except:
+            return False
     return True
 
 def moveMouseTo(x, y):
-    pyautogui.moveTo(100, 100, duration = 5)
-    print("debug: mouse moved to ({x}, {y})")
+    offsetX = 350
+    offsetY = 300
+    pyautogui.moveTo((x-offsetX), (y+offsetY), duration = 3)
+    print(f"debug: mouse moved to ({(x-offsetX)},{(y+offsetY)})")
 
 
 if __name__ == "__main__":
