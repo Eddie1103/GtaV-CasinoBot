3a559a33da3e5fe4259beecb9a0876459d349c5d
Eddie <89065499+Eddie1103@users.noreply.github.com>
2023-08-08 14:32:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,76 @@
 import pyautogui
-import time
+import time, os
+from screenSize import GetScreenSize
 
-_amount = 1000
 _betCounter = 1
+SCREEN_SIZE = GetScreenSize()
+SCREEN_STRING = str(SCREEN_SIZE[0]) + "x" + str(SCREEN_SIZE[1])
 
 def startup():
+    if os.path.exists("pics/bet" + SCREEN_STRING + ".png") == False:
+        print("Kein Referenzbild für aktuelle Screen-Size gefunden: " + SCREEN_STRING +"px")
+        exit()
     print("Casino-Bot is starting...")
-    
+
     while(True):
         while(findBetField() == False):
             print("Konnte Tisch nicht finden...")
             time.sleep(5)
-
+            '''
         while(detectWinOrLose() == False):
             print("Runde läuft momentan...")
             time.sleep(2)
+            '''
+
     exit()
 
+def get_jetons_coords():
+    coords = pyautogui.locateCenterOnScreen(f'pics/jetons{SCREEN_STRING}.png', confidence=0.8)
+    return coords
+
 def findBetField():
-    x, y= pyautogui.locateCenterOnScreen("redbet.png", region=(980,630,100,60))
-    if x != None:
-        pyautogui.moveTo(x, y, duration = 3)
-        pyautogui.press("tab")
+    # try:
+    #pyautogui.screenshot(imageFilename="test.png", region=(980,630,100,60))
+    coords = pyautogui.locateCenterOnScreen(f'pics/bet{SCREEN_STRING}.png', confidence=0.8)
+    if coords == None:
+        return False
+    print(coords)
+    coordsJetons = get_jetons_coords()
+    if coordsJetons == None:
+        return False
+    while coordsJetons != coords:
+        print(coordsJetons)
+        # calculate offset from jetons to betfield and move cursor by offset
+        offset = (coords[0] - coordsJetons[0], coords[1] -  coordsJetons[1])
+        currentMousePos = pyautogui.position()
+        pyautogui.moveTo(currentMousePos[0] + offset[0], currentMousePos[1] + offset[1])
+        coordsJetons = get_jetons_coords()
+        if coordsJetons == None:
+            return False
         time.sleep(0.5)
-        for i in range(_betCounter):
-            pyautogui.leftClick()
-            time.sleep(1)
-        return True
+    pyautogui.press("tab")
+    time.sleep(0.5)
+
     return False
+    for i in range(_betCounter):
+        pyautogui.leftClick()
+        time.sleep(1)
+    return True
+    # except:
+    #     return False
 
 def detectWinOrLose():
-    x, y= pyautogui.locateCenterOnScreen("win.png", region=(0,0,370,90))
-    if x == None:
-        x,y = pyautogui.locateCenterOnScreen("lose.png", region=(0,0,370,90))
-        if x == None:
-            return False
-        else:
-            _betCounter*2
-    else:
+    try:
+        x, y= pyautogui.locateCenterOnScreen("pics/win.png")
         _betCounter = 1
+        print("Gewonnen! Bet-Counter auf 1 gesetzt")
+    except:
+        try:
+            x,y = pyautogui.locateCenterOnScreen("pics/lose.png")
+            _betCounter*=2
+            print("Verloren! Bet-Counter verdoppelt auf " + _betCounter)
+        except:
+            return False
     return True
 
 def moveMouseTo(x, y):
